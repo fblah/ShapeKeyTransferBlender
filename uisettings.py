b89e8fc4979563ad505297dca265be8a8e393075
poor <christian.brinkmann@uni-weimar.de>
2021-07-09 10:54:04 +0200

    cleanup - annotations, context references, naming

--- uisettings.py
+++ uisettings.py
@@ -25,21 +25,18 @@ from bpy.props import (StringProperty,
                        BoolProperty,
                        IntProperty,
                        FloatProperty,
-                       FloatVectorProperty,
-                       EnumProperty,
-                       PointerProperty,
-                       )
-from bpy.types import (Panel,
-                       Operator,
-                       PropertyGroup,
-                       )
+                       PointerProperty)
 
-# Properties of the main part of the panel
+from bpy.types import PropertyGroup
+
+
+# Scene Properties
 # ----------------------------------------------------------
 
-class UISettings(PropertyGroup):
-    """Mesh Selection Boxes"""
-    src_mesh : PointerProperty(
+class SKT_PG_settings(PropertyGroup):
+    """SKT scene properties"""
+
+    src_mesh: PointerProperty(
         type=bpy.types.Mesh, 
         name="Source Mesh", 
         description="Select a source mesh", 
@@ -47,7 +44,7 @@ class UISettings(PropertyGroup):
         update=None
         )
     
-    dest_mesh : PointerProperty(
+    dest_mesh: PointerProperty(
         type=bpy.types.Mesh, 
         name="Destination Mesh", 
         description="Select a destination mesh", 
@@ -55,30 +52,25 @@ class UISettings(PropertyGroup):
         update=None
         )
 
-# Properties of the operator
-# ----------------------------------------------------------
-
-class TransferShapeKeysOperatorUI(PropertyGroup):
-    """Transfer shape key operator options and properties"""
-    specify_end_vertex : BoolProperty(
+    specify_end_vertex: BoolProperty(
         name="Specify End Vertex",
         description="Execute till last vertex specified.",
         default = False
         )
 
-    use_one_vertex : BoolProperty(
+    use_one_vertex: BoolProperty(
         name="Use Closest Vertex",
         description="Use the position of the closet vertex only or several vertices within the range.",
         default = True
         )
     
-    skip_unpaired_vertices : BoolProperty(
+    skip_unpaired_vertices: BoolProperty(
         name="Skip unpaired vertices",
         description="Use this to skip vertices which cant find naerby vertices in source mesh to copy position from.",
         default = True
         )
 
-    increment_radius : FloatProperty(
+    increment_radius: FloatProperty(
         name = "Increment Radius",
         description = "Radius to increment selection sphere.",
         default = 0.05,
@@ -87,7 +79,7 @@ class TransferShapeKeysOperatorUI(PropertyGroup):
         min = 0.00000001
         )
     
-    number_of_increments : IntProperty(
+    number_of_increments: IntProperty(
         name = "Number of increments",
         description = "Number of times to increment selection radius before giving up.",
         default = 2,
@@ -99,8 +91,8 @@ class TransferShapeKeysOperatorUI(PropertyGroup):
 # Property of 1 item in the excluded shape key list
 # ----------------------------------------------------------
 
-class ShapeKeyItem(PropertyGroup):
+class SKT_PG_shapeKeyListItem(PropertyGroup):
     """List box properties"""
-    #name = StringProperty() -> Instantiated by default    
-    obj_type = StringProperty()
-    obj_id = IntProperty()
+    #name : StringProperty() -> Instantiated by default    
+    obj_type: StringProperty()
+    obj_id: IntProperty()
